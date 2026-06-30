35667b3af2a42d4254a6c5a88531fbfb6a8b012b
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-06-30 16:42:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ void kernel_main() {
     unsigned char* video = (unsigned char*)0xb8000;
     char* mozi = "Hama OS [Version 1.0.0]";  
     int mozinagasa = stlen(mozi);
-    for (int i = 0; i < mozinagasa; i += 2) {
+    for (int i = 0; i < mozinagasa; i ++) {
         video[i*2] = mozi[i];   // 偶数番地には「文字コード」を書き込む
         video[i * 2 + 1] = 0x0F;      // 奇数番地には「色属性（白文字/黒背景）」を書き込む
     }
