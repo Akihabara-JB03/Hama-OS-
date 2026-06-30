ed2d3201ef4ce2188427bfa6433c04cc5055e4df
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-06-30 16:41:07 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ void kernel_main() {
     char* mozi = "Hama OS [Version 1.0.0]";  
     int mozinagasa = stlen(mozi);
     // 文字列の長さは24文字 ＝ 文字と色で合計48バイト分ループを回す
-    for (int i = 0; i < mozinagasa * 2; i += 2) {
-        video[i] = mozi[i / 2];   // 偶数番地には「文字コード」を書き込む
-        video[i + 1] = 0x0F;      // 奇数番地には「色属性（白文字/黒背景）」を書き込む
+    for (int i = 0; i < mozinagasa; i += 2) {
+        video[i*2] = mozi[i];   // 偶数番地には「文字コード」を書き込む
+        video[i * 2 + 1] = 0x0F;      // 奇数番地には「色属性（白文字/黒背景）」を書き込む
     }
 
     while (1) {
