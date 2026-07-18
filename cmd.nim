471fc25ff4c0f13d6b18200e1c8ce8b20e31045d
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-18 13:26:23 +0900

    Update cmd.nim

--- cmd.nim
+++ cmd.nim
@@ -1,12 +1,1 @@
-# io.h で作った関数をNimに教えてあげる（importする感覚）
-proc clear() {.importc: "clear".}
-proc print(str: cstring) {.importc: "print".}
 
-# コマンドを判定するNimの関数
-proc executeCommand(cmd: cstring) {.exportc: "executeCommand".} =
-  if cmd == "clear":
-    clear()
-  elif cmd == "hello":
-    print("Hello! This is Hama OS, powered by Nim!\n")
-  else:
-    print("Unknown command!\n")
