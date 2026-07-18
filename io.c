f1897bc43f1c202edd1ca98fd2abebe648004fe6
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-18 12:17:29 +0900

    Refactor print function for video output
    Refactor print function to include string length calculation and video memory writing.

--- io.c
+++ io.c
@@ -1,3 +1,18 @@
-func print(char* str) {
-  
+#include "str.h"
+void print(char* str) {
+  unsigned char* video = (unsigned char*)0xb8000;
+  int mozinagasa = stlen(str);
+  int cursor_x = 0;
+  int cursor_y = 0;
+  for (int i = 0; i<mozinagasa; i++) {
+    if (cursor_x == 80) {
+      cursor_y++;
+      cursor_x=0;
+    } else {
+      
+      video[(cursor_y*80+cursor_x)*2] = str[i];
+      video[(cursor_y*80+cursor_x)*2+1] = 0x07;
+      cursor_x++;
+    }
+  }
 }
