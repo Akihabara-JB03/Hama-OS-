586864cd526e9b20f83a09183074e1c5e296e4e9
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-18 13:11:43 +0900

    Update str.c

--- str.c
+++ str.c
@@ -5,3 +5,42 @@ int stlen(char* str) {
   }
   return i;
 }
+int H_strcmp(char *a,char *b) {
+    int i = 0;
+    while (a[i] != '\0' && b[i] != '\0') {
+        if (a[i] != b[i]) {
+            return 1;
+        } else {
+            i++;
+        }
+    }
+    if (a[i] == b[i]) {
+        return 0;
+    } else if (a[i] != b[i]) {
+        return 1;
+    }
+}
+char *H_strcpy(char *src, char *dst) {
+    int j = 0;
+    while (src[j] != '\0') {
+        dst[j] = src[j];
+        j++;
+    }
+    dst[j] = '\0';
+    return dst;
+}
+
+char *H_strcat(char *src,char *dst,int n) {
+    int k = 0;
+    int pos = strlen(dst);
+    while (src[k] != '\0') {
+        if ((pos + k) < n) {
+            dst[pos+k] = src[k];
+            k++;
+        } else {
+            break;
+        }
+    }
+    dst[pos+k] = '\0';
+    return dst;
+}
