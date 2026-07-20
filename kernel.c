d086c7ce96c6ce43642c53bfaa52973d4982fb6e
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-20 12:01:21 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -5,7 +5,31 @@
 void kernel_main() {
     clear();
     print("HAMA OS [VER 0.01.2] HELLO WORLD");
-    
+    void execute_command(void) {
+        char line[81]; // 画面1行分（80文字）＋ 終わりの '\0'（1文字）が入る箱
+        int x;
+
+        // 【ステップ1】今の行（cursor_y）の文字を、左端（0）から右端（79）まで集めるループ
+        for (x = 0; x < 80; x++) {
+            // あなたが作った current_char(x, cursor_y) を使って、1文字ずつ line[x] にしまう！
+            line[x] = current_char(x, cursor_y);
+        }
+        // ループが終わったら、文字列のルールとして最後に必ず終了の印を入れる
+        line[x] = '\0';
+        if (H_strcmp(line, "hello") == 0) {
+            print("\nHello! This is HAMA OS!\n");
+        }     
+        else if (H_strcmp(line, "clear") == 0) {
+            clear();
+        } 
+        else {
+            print("\nUnknown Command...\n");
+        }
+
+        // 【ステップ3】次の入力のために、改行してプロンプトを表示する
+        print("HAMA_OS> ");
+    }
+
     while (1) {
         // 無限ループで待機
     }
