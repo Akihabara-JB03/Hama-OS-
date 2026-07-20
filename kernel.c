7aa8eadc0de688959917c2f6dbe7573a30a88485
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-20 11:36:48 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,8 +1,9 @@
 #include "io.h"
-#include　"interrupt.h"
+#include　"keyboard.h"
 void kernel_main() {
     clear();
     print("HAMA OS [VER 0.01.2] HELLO WORLD");
+    
     while (1) {
         // 無限ループで待機
     }
