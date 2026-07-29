c7e537f6aec8321bcb5dc1c70b06bc7fa84bb4bf
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-29 22:32:47 +0900

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -25,29 +25,6 @@ void execute_command(void) {
 
     // 【ステップ3】次の入力のために、改行してプロンプトを表示する
     print("HAMA_OS> ");
-        
-}
-// キーボードが押された瞬間に、自動で呼び出される関数
-void keyboard_handler_main(void) {
-    unsigned char code = in8(0x60);
-    if (code < 0x80) {
-        char ascii = scan_to_ascii[code];
-        if (ascii != 0) {
-            
-            // 【ここであなたが条件分岐を1つ足す！】
-            // もし、今押された文字（ascii）がエンターキー（'\n'）だったら、
-            // ここで『execute_command();』を呼び出す！
-            if (ascii == '\n') {
-                execute_command();
-            }
-            // エンターじゃなくて、普通の文字だったら画面に出す
-            else {
-                char str[2] = {ascii, '\0'};
-                print(str);
-            }
-        }
-    }
-    out8(0x20, 0x20);
 }
 
 void kernel_main() {
